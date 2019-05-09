0226e51d73dd6c599c8b0297ab3350bcf754e057
Hung Nguyen <nguyendanghung2008@gmail.com>
2019-05-09 03:35:33 +0100

    update miro service client

--- src/main/java/io/cooly/crawler/client/FetcherServiceClient.java
+++ src/main/java/io/cooly/crawler/client/FetcherServiceClient.java
@@ -3,10 +3,15 @@ package io.cooly.crawler.client;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @AuthorizedFeignClient(name = "fetcher")
-public interface WebUrlClient {
+public interface FetcherServiceClient {
+    
     @RequestMapping("/api/web-urls")
     List<WebUrl> getFetches();
+    
+    @PostMapping("api/web-urls")
+    WebUrl createWebUrl(WebUrl webUrl);
 }
--- src/main/java/io/cooly/crawler/service/Scheduler.java
+++ src/main/java/io/cooly/crawler/service/Scheduler.java
@@ -3,7 +3,6 @@ package io.cooly.crawler.service;
 
 
 import io.cooly.crawler.client.WebUrl;
-import io.cooly.crawler.client.WebUrlClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,12 +10,13 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import io.cooly.crawler.client.FetcherServiceClient;
 
 @Component
 public class Scheduler {
     private final Logger log = LoggerFactory.getLogger(Scheduler.class);
     @Autowired
-    private WebUrlClient webUrlClient;
+    private FetcherServiceClient webUrlClient;
     @Scheduled(fixedRate = 1000)
     public void fixedRateSch() {
         List<WebUrl> fetchList = webUrlClient.getFetches();
